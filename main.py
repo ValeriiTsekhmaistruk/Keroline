e17af14be165885bde961ac71b9ae00001b0059f
Alan Miller <valeriitseh1305@gmail.com>
2021-09-20 20:15:52 +0300

    Добавил модули времени, погоды и математического корня. Оптимизировал код в приведущих модулях

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import sys
 import webbrowser
-#from pyowm import OWM
+import math
+import time
+
+
+class Assistant:
+    name = 'Керолайн'
+    city = 'Николаев'
 
 
 class Commands:
@@ -10,61 +16,17 @@ class Commands:
 
     @staticmethod
     def communication(text):  # общение
-        hello = 'привет'
-        goodbye = 'пока'
-        thanks = 'спасибо'
-        how_are_you = 'дела'
 
         for word in text:
-            if word in hello:
+            if word in 'привет':
                 print('привет')
-            elif word in goodbye:
+            elif word in 'пока':
                 print('пока')
                 sys.exit()
-            elif word in thanks:
+            elif word in 'спасибо':
                 print('пожалуйста')
-            elif word in how_are_you:
+            elif word in 'дела':
                 print('отлично')
-
-    @staticmethod
-    def math(text):  # математика
-        black_list = ('на', 'пожалуйста', 'сколько', 'будет')
-        for word in text:
-            if word in black_list:
-                text.remove(word)
-
-        num = []
-        multi = ('умнож', 'умножить', '*')
-        division = ('подели', 'раздели', '/')
-        summ = ('плюс', '+')
-        sub = ('минус', '-')
-
-        try:
-            for word in text:
-                if word.isdigit():
-                    num.append(int(word))
-            text.remove(str(num[0]))
-            text.remove(str(num[1]))
-        except IndexError:
-            print('Ошибка в данних')
-            return
-        if text[0] in multi:
-            res = num[0] * num[1]
-            print(res)
-        elif text[0] in division:
-            res = 0
-            try:
-                res = num[0] / num[1]
-            except ZeroDivisionError:
-                print('Нельзя делить на 0!')
-            else:
-                print(res)
-        elif text[0] in summ:
-            res = num[0] + num[1]
-            print(res)
-        elif text[0] in sub:
-            res = num[0] - num[1]
-            print(res)
 
     @staticmethod
     def search_google(text):  # поиск в google
@@ -74,21 +36,84 @@ class Commands:
         print('вот что есть:')
         webbrowser.get().open(url)
 
-    #@staticmethod
-    #def get_weather(text):
-    #    black_list = ('погода', 'в')
-    #    for word in text:
-    #        print(text)
-    #        if word in black_list:
-    #            text.remove(word)
-    #    print(text)
+    @staticmethod
+    def say_time(text):
+        black_list = ('скажи', 'пожалуйста', 'который', 'какой',
+                      'сейчас', 'какое', 'который', 'сколько',
+                      'сегодня', 'который', 'назови', 'недели')
+        clear_text = list(filter(lambda item: item not in black_list, text))
+
+        if clear_text[0] in ('время', 'времени', 'час', 'часов'):
+            print(time.strftime('%H:%M', time.localtime()))
+        elif clear_text[0] in ('дата', 'число', 'дату'):
+            print(time.strftime('%m.%d.%Y', time.localtime()))
+        elif clear_text[0] in ('день'):
+            print(time.strftime('%A', time.localtime()))
+
+    @staticmethod
+    def get_weather(text):
+        black_list = ('покажи', 'прогноз', 'в', 'погода', 'погоду', 'погоды')
+        clear_text = list(filter(lambda item: item not in black_list, text))
+
+        if len(clear_text) == 0:
+            url = 'https://google.com/search?q=' + f'погода  в {Assistant.city}'
+            webbrowser.get().open(url)
+        else:
+            url = 'https://google.com/search?q=' + f'погода  в {clear_text[0]}'
+            webbrowser.get().open(url)
+
+
+class Mathematics:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def simple_math(text):  # математика
+        black_list = ('на', 'пожалуйста', 'сколько', 'будет')
+        clear_text = list(filter(lambda item: item not in black_list, text))
+        num = []
+
+        try:
+            for word in clear_text:
+                if word.isdigit():
+                    num.append(int(word))
+            clear_text.remove(str(num[0]))
+            clear_text.remove(str(num[1]))
+        except IndexError:
+            print('Ошибка в данних')
+            return
+        if clear_text[0] in ('умнож', 'умножить', '*'):
+            res = num[0] * num[1]
+            print(res)
+        elif clear_text[0] in ('подели', 'раздели', '/'):
+            res = 0
+            try:
+                res = num[0] / num[1]
+            except ZeroDivisionError:
+                print('Нельзя делить на 0!')
+            else:
+                print(res)
+        elif clear_text[0] in ('плюс', '+'):
+            res = num[0] + num[1]
+            print(res)
+        elif clear_text[0] in ('минус', '-'):
+            res = num[0] - num[1]
+            print(res)
+
+    @staticmethod
+    def math_sqrt(text):
+        num = list(filter(lambda x: x.isdigit(), text))
+        print(math.sqrt(int(num[0])))
 
 
 commands = {
     ('привет', 'пока', 'спасибо', 'дела'): Commands.communication,
-    ('+', '-', '*', '/', 'плюс', 'минус', 'умнож', 'умножить', 'подели', 'раздели'): Commands.math,
     ('найди', 'поищи'): Commands.search_google,
-    #('погода'): Commands.get_weather
+    ('время', 'времени', 'час', 'часов', 'дата', 'число', 'дату', 'день'): Commands.say_time,
+    ('погода', 'погоду', 'погоды'): Commands.get_weather,
+    ('+', '-', '*', '/', 'плюс', 'минус', 'умнож', 'умножить', 'подели', 'раздели'): Mathematics.simple_math,
+    'корень': Mathematics.math_sqrt,
     }
 
 
