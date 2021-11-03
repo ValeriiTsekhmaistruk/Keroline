93d832c9df8c0ff38cd155cfc827573d09f262ba
Alan Miller <valeriitseh1305@gmail.com>
2021-11-03 21:30:14 +0200

    K

---
+++ src/app.py
@@ -0,0 +1,170 @@
+import main
+import json
+import sys
+import os
+from kivy.app import App
+from kivy.config import Config
+Config.set('graphics', 'resizable', 0)
+Config.set('graphics', 'width', '300')
+Config.set('graphics', 'height', '440')
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.uix.popup import Popup
+from kivy.uix.checkbox import CheckBox
+
+Window.clearcolor = (40/225, 40/225, 40/225, 1)
+
+
+class KerolineApp(App):
+
+    def build(self):
+        main_bl = BoxLayout(orientation='vertical',
+                            padding=10,
+                            spacing=5)
+
+        setting_menu_content_bl = BoxLayout(orientation='vertical')
+        setting_menu = Popup(title='Настройки', content=setting_menu_content_bl, auto_dismiss=False)
+
+        label_bl = BoxLayout(size_hint=(1, .06))
+        text_bl = BoxLayout(size_hint=(1, .48))
+        send_bl = BoxLayout(size_hint=(1, .08), spacing=3)
+        speak_btn_bl = BoxLayout(size_hint=(1, .29), padding=(80, 4, 80, 4))
+        setting_btn_bl = BoxLayout(size_hint=(1, .09))
+
+        name_label = Label(text='KEROLINE',
+                           font_size=30)
+
+        self.text_label = Label(text='Cлушаю!', font_size=20,
+                                text_size=(300 - 20, 400 * .4 - 20),
+                                valign='center',
+                                halign='center')
+
+        self.send_input = TextInput(size_hint=(.8, 1),
+                                    font_size=15,
+                                    multiline=False)
+
+        self.send_btn = Button(size_hint=(.13, 1),
+                               background_normal='..//img//send.png',
+                               background_down='..//img//send_on.png',
+                               border=(0, 0, 0, 0))
+
+        self.speak_btn = Button(background_normal='..//img//mic.png',
+                                background_down='..//img//mic_on.png',
+                                border=(0, 0, 0, 0))
+
+        self.setitng_btn = Button(text='Настройки')
+
+        label_bl.add_widget(name_label)
+        text_bl.add_widget(self.text_label)
+
+        send_bl.add_widget(self.send_input)
+        send_bl.add_widget(self.send_btn)
+
+        speak_btn_bl.add_widget(self.speak_btn)
+
+        setting_btn_bl.add_widget(self.setitng_btn)
+
+        main_bl.add_widget(label_bl)
+        main_bl.add_widget(text_bl)
+        main_bl.add_widget(send_bl)
+        main_bl.add_widget(speak_btn_bl)
+        main_bl.add_widget(setting_btn_bl)
+
+        setting_menu_bl = BoxLayout(size_hint=(1, .9),
+                                    orientation='vertical',
+                                    padding=5,
+                                    spacing=1)
+        setting_menu_btn_bl = BoxLayout(size_hint=(1, .1))
+
+        self.city = TextInput(font_size=11, multiline=False, size_hint=(1, .27), text=main.Assistant.city)
+        self.path_spotify = TextInput(font_size=11,multiline=False, size_hint=(1, .27), text=main.Assistant.spotify)
+        self.path_telegram = TextInput(font_size=11, multiline=False, size_hint=(1, .27), text=main.Assistant.telegram)
+        self.path_browser = TextInput(font_size=11, multiline=False, size_hint=(1, .27), text=main.Assistant.browser)
+        self.path_office = TextInput(font_size=11, multiline=False, size_hint=(1, .27), text=main.Assistant.office)
+        self.voice_cb = CheckBox(size_hint=(1, .27))
+        self.voice_cb.active = main.Assistant.voice
+
+        self.close_setting_btn = Button(text='Выйти')
+        self.save_setting_btn = Button(text='Сохранить')
+
+        city_label = Label(text='Ваш город', font_size=12, size_hint=(1, .3), halign='left', text_size=(265, 20))
+        spotify_label = Label(text='Spotify', font_size=12, size_hint=(1, .3), halign='left', text_size=(265, 20))
+        telegram_label = Label(text='Telegram', font_size=12, size_hint=(1, .3), halign='left', text_size=(265, 20))
+        browser_label = Label(text='Браузер', font_size=12, size_hint=(1, .3), halign='left', text_size=(265, 20))
+        office_label = Label(text='Office', font_size=12, size_hint=(1, .3), halign='left', text_size=(265, 20))
+        voice_label = Label(text='Голос', font_size=15, size_hint=(1, .25))
+
+        setting_menu_bl.add_widget(city_label)
+        setting_menu_bl.add_widget(self.city)
+        setting_menu_bl.add_widget(spotify_label)
+        setting_menu_bl.add_widget(self.path_spotify)
+        setting_menu_bl.add_widget(telegram_label)
+        setting_menu_bl.add_widget(self.path_telegram)
+        setting_menu_bl.add_widget(browser_label)
+        setting_menu_bl.add_widget(self.path_browser)
+        setting_menu_bl.add_widget(office_label)
+        setting_menu_bl.add_widget(self.path_office)
+        setting_menu_bl.add_widget(voice_label)
+        setting_menu_bl.add_widget(self.voice_cb)
+
+        setting_menu_btn_bl.add_widget(self.save_setting_btn)
+        setting_menu_btn_bl.add_widget(self.close_setting_btn)
+
+        setting_menu_content_bl.add_widget(setting_menu_bl)
+        setting_menu_content_bl.add_widget(setting_menu_btn_bl)
+
+        def enter_on(instance):
+            answer = main.input_text(instance.text)
+            if answer:
+                self.text_label.text = str(answer)
+            instance.text = ''
+
+        def send_btn_on(instance):
+            answer = main.input_text(self.send_input.text)
+            if answer:
+                self.text_label.text = str(answer)
+            self.send_input.text = ''
+
+        def speak_btn_on(instance):
+            answer = main.input_voice()
+            if answer:
+                self.text_label.text = str(answer)
+
+        def save_setting_btn_on(instance):
+            if self.voice_cb.active:
+                voice_value = '1'
+            else:
+                voice_value = ''
+
+            data = {
+                "city": self.city.text,
+                "voice": voice_value,
+
+                "spotify": self.path_spotify.text.strip(),
+                "telegram": self.path_telegram.text.strip(),
+                "browser": self.path_browser.text.strip(),
+                "office": self.path_office.text.strip()
+            }
+
+            with open("config.json", "w", encoding='utf-8') as write_file:
+                json.dump(data, write_file, indent=4, ensure_ascii=False)
+
+            python = sys.executable
+            os.execl(python, python, "\"{}\"".format(sys.argv[0]))
+
+        self.send_input.bind(on_text_validate=enter_on)
+        self.send_btn.bind(on_press=send_btn_on)
+        self.speak_btn.bind(on_release=speak_btn_on)
+        self.setitng_btn.bind(on_press=setting_menu.open)
+        self.close_setting_btn.bind(on_press=setting_menu.dismiss)
+        self.save_setting_btn.bind(on_press=save_setting_btn_on)
+
+        return main_bl
+
+
+if __name__ == '__main__':
+    main.Voice.say('слушаю!')
+    KerolineApp().run()
---
+++ src/main.py
@@ -0,0 +1,351 @@
+import os
+import sys
+import webbrowser
+import math
+import time
+import random
+import json
+import pyttsx3
+import threading
+import speech_recognition
+from collections import namedtuple
+
+
+class Assistant:
+    with open("config.json", "r", encoding='utf-8') as file:
+        data = json.load(file)
+
+    city = data.get('city')
+    voice = bool(data.get('voice'))
+
+    spotify = data.get('spotify')
+    telegram = data.get('telegram')
+    browser = data.get('browser')
+    office = data.get('office')
+
+    answers = {
+        'hi': ('Здраствуй!', 'Приветствую!', 'Здраствуйте!', 'Доброго времени суток!'),
+        'bye': ('До встречи!', 'Прощайте!', 'До новых встреч!', 'До свидания!'),
+        'grat': ('Пожалуйста!', 'Всегда пожалуйста!', 'Обращайтесь!', 'Рада помочь!'),
+        'execution': ('Выполняю!', 'Прошу!', 'Конечно!')
+    }
+
+    answer = ''
+
+
+class Voice:
+
+    @staticmethod
+    def say(string):
+
+        def engine_start(text):
+            if Assistant.voice:
+                engine = pyttsx3.init()
+                engine.say(text)
+                engine.runAndWait()
+            else:
+                return
+
+        start = threading.Thread(target=engine_start, args=[string])
+        start.start()
+
+    @staticmethod
+    def say_num(string):
+        if string % 1 != 0:
+            result = round(string, 2)
+            split_num = str(result).split('.')
+            Voice.say(f'{split_num[0]},{split_num[1]}')
+        else:
+            Voice.say(int(string))
+
+    @staticmethod
+    def listen():
+        mic = speech_recognition.Microphone()
+        recognizer = speech_recognition.Recognizer()
+
+        with mic:
+            try:
+                audio = recognizer.listen(mic, 5, 5)
+                result = recognizer.recognize_google(audio, language="ru-RU").lower()
+                return result
+            except speech_recognition.UnknownValueError:
+                Assistant.answer = 'Повторите пожалуйста'
+                Voice.say('Повторите пожалуйста')
+                return ''
+            except speech_recognition.RequestError:
+                Assistant.answer = 'Проверьте подключение к интернету'
+                Voice.say('Проверьте подключение к интернету')
+                return ''
+            except speech_recognition.WaitTimeoutError:
+                Assistant.answer = 'Проверьте микрофон'
+                Voice.say('Проверьте микрофон')
+                return ''
+
+
+class Commands:
+
+    @staticmethod
+    def communication(text):
+        hi_key_words = ('привет', 'приветствую')
+        bye_key_words = ('пока', 'прощай')
+        grat_key_words = ('спасибо', 'благодарю')
+        info_key_words = ('кто', '  ')
+        command_key_words = ('делаешь', 'умеешь', 'можешь', 'ты')
+
+        if bool(set(hi_key_words) & set(text)):
+            answer = random.choice(Assistant.answers.get('hi'))
+            Assistant.answer = answer
+            Voice.say(answer)
+        elif bool(set(bye_key_words) & set(text)):
+            answer = random.choice(Assistant.answers.get('bye'))
+            Voice.say(answer)
+            sys.exit()
+        elif bool(set(grat_key_words) & set(text)):
+            answer = random.choice(Assistant.answers.get('grat'))
+            Assistant.answer = answer
+            Voice.say(answer)
+        elif bool(set(info_key_words) & set(text)):
+            Assistant.answer = f'Я Кэролайн, ваш личный голосовой ассистент'
+            Voice.say(f'Я Кэролайн, ваш личный голосовой ассистент')
+        elif bool(set(command_key_words) & set(text)):
+            Assistant.answer = 'Затрудняюсь ответить, меня постоянно улучшают'
+            Voice.say('Затрудняюсь ответить, меня постоянно улучшают')
+
+    @staticmethod
+    def search_google(text):
+        if len(text) == 1:
+            Assistant.answer = 'Что именно вы хотите найти?'
+            Voice.say('Что именно вы хотите найти?')
+            return
+        text.pop(0)
+        search_query = ' '.join(text)
+        url = 'https://google.com/search?q=' + search_query
+        answer = random.choice(Assistant.answers.get('execution'))
+        Assistant.answer = answer
+        Voice.say(answer)
+        webbrowser.get().open(url)
+
+    @staticmethod
+    def say_time(text):
+        time_word = ('время', 'времени', 'час', 'часов')
+        full_date_word = ('дата', 'дату')
+
+        if bool(set(time_word) & set(text)):
+            Assistant.answer = time.strftime('%H:%M', time.localtime())
+            Voice.say(time.strftime('%H:%M', time.localtime()))
+        elif bool(set(full_date_word) & set(text)):
+            Assistant.answer = time.strftime('%d.%m.%Y', time.localtime())
+            Voice.say(time.strftime('%d/%m/%Y', time.localtime()))
+        elif 'число' in text:
+            Assistant.answer = time.strftime('%m.%d', time.localtime())
+            Voice.say(time.strftime('%m/%d', time.localtime()))
+        elif 'день' in text:
+            Assistant.answer = time.strftime('%A', time.localtime())
+            Voice.say(time.strftime('%A', time.localtime()))
+
+    @staticmethod
+    def get_weather(text):
+        black_list = ('покажи', 'прогноз', 'в', 'погода', 'погоду', 'погоды', 'пожалуйста', 'мне')
+        clear_text = list(filter(lambda item: item not in black_list, text))
+
+        if len(clear_text) == 0 and Assistant.city == '':
+            Assistant.answer = 'Укажите город'
+            Voice.say('Укажите город')
+        elif len(clear_text) == 0:
+            answer = random.choice(Assistant.answers.get('execution'))
+            Assistant.answer = answer
+            Voice.say(answer)
+            url = 'https://google.com/search?q=' + f'погода  в {Assistant.city}'
+            webbrowser.get().open(url)
+        else:
+            answer = random.choice(Assistant.answers.get('execution'))
+            Assistant.answer = answer
+            Voice.say(answer)
+            url = 'https://google.com/search?q=' + f'погода  в {clear_text[0]}'
+            webbrowser.get().open(url)
+
+    @staticmethod
+    def run_app(text):
+        if len(text) < 2:
+            Assistant.answer = 'Укажите приложение'
+            Voice.say('Укажите приложение')
+            return
+
+        app_list = ('telegram', 'spotify', 'офис', 'браузер')
+        clear_text = list(filter(lambda item: item in app_list, text))
+
+        if len(clear_text) == 0:
+            Assistant.answer = 'Приложение не найденно'
+            Voice.say('Приложение не найденно')
+            return
+
+        def sey(x):
+            if x:
+                answer = random.choice(Assistant.answers.get('execution'))
+                Assistant.answer = answer
+                Voice.say(answer)
+            else:
+                Assistant.answer = 'Приложение не установленно'
+                Voice.say('Приложение не установленно')
+        try:
+            if clear_text[0] == 'spotify':
+                if Assistant.spotify == '':
+                    sey(False)
+                    return
+                os.startfile(Assistant.spotify)
+                sey(True)
+
+            if clear_text[0] == 'telegram':
+                if Assistant.telegram == '':
+                    sey(False)
+                    return
+                os.startfile(Assistant.telegram)
+                sey(True)
+
+            if clear_text[0] == 'офис':
+                if Assistant.office == '':
+                    sey(False)
+                    return
+                os.startfile(Assistant.office)
+                sey(True)
+
+            if clear_text[0] == 'браузер':
+                if Assistant.browser == '':
+                    sey(False)
+                    return
+                os.startfile(Assistant.browser)
+                sey(True)
+        except FileNotFoundError:
+            Assistant.answer = 'Указан неверный путь к приложению'
+            Voice.say('Указан неверный путь к приложению')
+
+
+class Mathematics:
+
+    @staticmethod
+    def simple_math(text):
+        multi_word = ('умнож', 'умножить', '*', 'х', 'x')
+        div_word = ('подели', 'раздели', '/')
+        sum_word = ('плюс', '+')
+        sub_word = ('минус', '-')
+        num = list(map(lambda x: x.replace(',', '.'), text))
+        num = list(map(float, filter(lambda x: x.replace('.', '').isdigit(), num)))
+        if len(num) == 1:
+            Assistant.answer = 'Не хватает аргумента'
+            Voice.say('Не хватает аргумента')
+            return
+        if len(num) == 0:
+            Assistant.answer = 'Укажите аргументы'
+            Voice.say('Укажите аргументы')
+            return
+
+        result = ''
+
+        if bool(set(multi_word) & set(text)):
+            result = (num[0] * num[1])
+        elif bool(set(div_word) & set(text)):
+            if num[1] == 0:
+                Assistant.answer = 'Делить на 0 нельзя!!!'
+                Voice.say('Делить на 0 нельзя!')
+                return
+            else:
+                result = (num[0] / num[1])
+        elif bool(set(sum_word) & set(text)):
+            result = (num[0] + num[1])
+        elif bool(set(sub_word) & set(text)):
+            result = (num[0] - num[1])
+
+        if result % 1 == 0:
+            Assistant.answer = round(result)
+        else:
+            Assistant.answer = result
+        Voice.say_num(result)
+
+    @staticmethod
+    def math_sqrt(text):
+        try:
+            num = list(map(float, filter(lambda x: x.replace('.', '').isdigit(), text)))
+            result = math.sqrt(num[0])
+
+            if result % 1 == 0:
+                Assistant.answer = round(result)
+            else:
+                Assistant.answer = result
+            Voice.say_num(result)
+
+        except IndexError:
+            Assistant.answer = 'Укажите аргумент'
+            Voice.say('Укажите аргумент')
+            return
+
+    @staticmethod
+    def math_exp(text):
+        try:
+            num = list(map(float, filter(lambda x: x.replace('.', '').isdigit(), text)))
+
+            if 'квадрат' in text:
+                result = (num[0]**2)
+            elif 'куб' in text:
+                result = (num[0]**3)
+            else:
+                result = (num[0]**num[1])
+
+            if result % 1 == 0:
+                Assistant.answer = round(result)
+            else:
+                Assistant.answer = result
+            Voice.say_num(result)
+
+        except IndexError:
+            Assistant.answer = 'Укажите аргумент'
+            Voice.say('Укажите аргумент')
+            return
+
+
+class KeyWord:
+    key_word = namedtuple('key_word', 'word func')
+
+    communication = key_word(('привет', 'приветствую', 'пока', 'прощай', 'спасибо', 'благодарю', 'делаешь',
+                              'умеешь', 'можешь', 'кто', 'ты'), Commands.communication)
+    search_google = key_word(('найди', 'поищи'), Commands.search_google)
+    say_time = key_word(('время', 'времени', 'час', 'часов', 'дата', 'число', 'дату', 'день'), Commands.say_time)
+    get_weather = key_word(('погода', 'погоду', 'погоды', 'прогноз'), Commands.get_weather)
+    simple_math = key_word(('+', '-', '*', '/', 'плюс', 'минус', 'умножь', 'умножить',
+                            'подели', 'раздели', 'х', 'x'), Mathematics.simple_math)
+    math_sqrt = key_word(('корень', 'корень'), Mathematics.math_sqrt)
+    math_exp = key_word(('степень', 'степени', 'квадрат', 'куб'), Mathematics.math_exp)
+    run_app = key_word(('включи', 'запусти', 'открой'), Commands.run_app)
+
+    key_words = (communication, search_google, say_time, get_weather, simple_math, math_sqrt, math_exp, run_app)
+
+
+def search_command(command):
+    text = command.lower().split(" ")
+    if text[0] == '':
+        return
+
+    def search(words, i=0):
+        if i <= len(words) - 1:
+            if set(words[i][0]) & set(text):
+                words[i][1](text)
+            else:
+                search(words, i+1)
+        else:
+            Assistant.answer = 'Неизвестная команда'
+            Voice.say('Неизвестная команда')
+            return
+    search(KeyWord.key_words)
+
+
+def input_text(user_command):
+    if len(user_command.replace(' ', '')) == 0:
+        return
+    search_command(user_command)
+    print(Assistant.voice)
+    return Assistant.answer
+
+
+def input_voice():
+    user_command = Voice.listen()
+    search_command(user_command)
+    return Assistant.answer
